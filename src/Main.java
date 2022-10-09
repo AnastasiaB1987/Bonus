c0c167aea85562eb8e63ae8767d41e6489654970
Anastasia Kliuchnikova <anastasia.kluch@gmail.com>
2022-10-09 20:46:48 +0300

    Testability

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int startCash = 1000;
-        int plus = 700;
-        int account = (startCash + plus);
-        int bonus = (account / 100 + 100);
-
-        if (account < 1000) {
-            System.out.println(account);
-        } else {
-            System.out.println(bonus + account);
-        }
-
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
